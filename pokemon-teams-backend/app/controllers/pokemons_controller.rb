3bb9c5262dd71c49001cfe86aefc99e543f21eb4
Pwako11 <akolo@hotmail.com>
2021-03-19 21:13:09 -0400

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,42 @@
+require 'pry'
 class PokemonsController < ApplicationController
+    def index
+        pokemons = Pokemon.all 
+        # options = {
+        #     include: [:pokemons]
+        # }
+        render json: pokemons
+        # render json: TrainerSerializer.new(trainers, options)
+    end 
+    
+    
+    def show 
+        pokemon = Pokemon.find_by(params[:id])
+        # options = {
+        #     include: [:pokemons]
+        # }
+        render json: pokemon
+        # render json: TrainerSerializer.new(trainer, options)
+    end 
+
+
+    def create
+        trainer = Trainer.find(params[:trainer_id])
+        pokemon = trainer.pokemons.build({
+            nickname: Faker::Name.first_name,
+            species: Faker::Games::Pokemon.name
+        })
+
+        if pokemon.save
+             render json: pokemon 
+        else 
+            render json: {message: pokemon.errors.messages[:team_max][0]}
+        end 
+        
+    end 
+
+    def destroy
+        pokemon = Pokemon.find(params[:id])
+        pokemon.destroy
+    end 
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -21,34 +21,77 @@ function renderTrainer(trainer){
     const ul = document.createElement('ul')
     const button = document.createElement('button')
     
-
     div.setAttribute("class", "card")
     div.setAttribute("data_id", trainer.id)
     p.innerText = trainer.name
 
     button.setAttribute("data-trainer-id", trainer.id)
     button.innerText = "Add Pokemon"
+    button.addEventListener("click", createPokemon )
+    // attach event listener to button (click)
 
     trainer.pokemons.forEach(pokemon => renderPokemon(pokemon))
     div.appendChild(p)
     div.appendChild(button)
     div.appendChild(ul)
     mainDiv.appendChild(div)
-        
+    
     function renderPokemon(pokemon){
-        console.log(pokemon)
-        const ul = document.querySelector(`div[data-id="${pokemon.trainer_id}"]`)
+            // console.log(pokemon)
+        // const ul = document.createElement('ul')
+        // const ul = document.querySelector(`div[data-id="${pokemon.trainer_id}"]`)
         const li = document.createElement('li')
         const button = document.createElement('button')
-        
+            // attach event listener to button (click)
+            
         button.setAttribute("class", "release")
         button.setAttribute("data-pokemon-id", pokemon.id)
         button.innerHTML = "Release"
+        button.addEventListener("click", deletePokemon )
         li.innerHTML =  `${pokemon.nickname} (${pokemon.species})`
         li.appendChild(button)
         ul.appendChild(li)
+        div.appendChild(ul)
     }
 
+
+    function createPokemon(e) {
+        e.preventDefault()
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json",
+                "Accept" : "application/json" 
+            },
+            body: JSON.stringify({trainer_id: e.target.dataset.trainerId})
+        }
+    
+        fetch(POKEMONS_URL, configObj)
+        .then( resp => resp.json())
+        .then( json => {
+           if (json.message){
+               alert(json.message)
+            } 
+           else { 
+            renderPokemon(json)
+            }       
+        })
+    }
+}
     
 
+
+function deletePokemon(e) {
+    e.preventDefault()
+    
+    const configObj = {
+        method: "DELETE",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json" 
+        },
+    }
+
+    fetch(`${POKEMONS_URL}/${e.target.dataset.pokemonId}`, configObj)
+    e.target.parentElement.remove()
 }
